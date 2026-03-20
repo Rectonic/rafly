3272a6581731ae93092b603029ce4e0931e2e499
Rectonic <boiskattakhodjaev@gmail.com>
2026-03-20 16:06:50 +0500

    fix: stub template files, fix jest setupFilesAfterEnv, add react-navigation to transform

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFiles: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|expo|@expo|@unimodules|lucide-react-native)',
+    'node_modules/(?!(jest-)?react-native|@react-native|expo|@expo|@unimodules|lucide-react-native|@react-navigation)',
   ],
 };
